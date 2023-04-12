2965cde9952b842e0cacf6606aa76b347bfe3829
emineksknc <eminekskncc@gmail.com>
2023-04-12 03:19:44 +0300

    Sqlite Example-update preload.js

--- electron-test/main.js
+++ electron-test/main.js
@@ -1,25 +1,18 @@
-const {BrowserWindow, app} = require('electron');
-const  {path} = require('path');
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, './preload.js'),
-            nodeIntegration: true
-        }
-    })
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  })
 
-    win.webContents.openDevTools()
-
-    win.loadFile('index.html')
+  win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-    createWindow();
-})
-
-app.on('window-all-closed', () => {
-    if(process.platform !== 'darwin') app.quit()
+  createWindow()
 })
